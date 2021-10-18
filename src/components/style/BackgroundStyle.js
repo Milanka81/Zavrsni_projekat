8de7158dee7d576ce1de3e87f555fb819ad20b87
Milanka81 <59183068+Milanka81@users.noreply.github.com>
2021-10-18 16:15:18 +0200

    Create BackgroundStyle.js

---
+++ src/components/style/BackgroundStyle.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+
+
+
+export default styled.div`
+    
+    background: linear-gradient(135deg, #be2e95, #f80759);
+    width: 100%;
+    min-height: 90vh;
+    display: flex;
+    justify-content: center;
+    align-items: center`
