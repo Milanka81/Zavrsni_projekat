e9068b0177c6e3dea5d2e559415c2d85dc87d0b2
Milanka81 <59183068+Milanka81@users.noreply.github.com>
2021-10-18 16:13:24 +0200

    Create validation.js

---
+++ src/components/validation.js
@@ -0,0 +1,5 @@
+export const isValidUsername = (username) => username.length >= 3
+export const isUsernameEmpty = (username) => username.trim() ===''
+export const isEmailEmpty =(email) => email.trim() ===''
+export const isPasswordEmpty =(password) => password.trim() ===''
+export const isValidPassword = (password) => password.length >=8
